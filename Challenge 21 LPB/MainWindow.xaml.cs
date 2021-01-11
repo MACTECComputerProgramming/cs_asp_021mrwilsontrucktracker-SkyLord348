537aff93e5c0661a28095971178d6d45bf08033d
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2021-01-11 13:20:20 -0600

    final commit

--- Challenge 21 LPB/MainWindow.xaml.cs
+++ Challenge 21 LPB/MainWindow.xaml.cs
@@ -26,18 +26,26 @@ namespace Challenge_21_LPB
         }
 
         public DateTime dateLeft, dateReturned;
+
         public void dateLeftCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
 
+
             dateLeft = dateLeftCalendar.SelectedDate.Value;
 
+            
+            
+        }
 
+        public void dateReturnedCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dateReturned = dateReturnedCalendar.SelectedDate.Value;
+
+            
         }
 
 
-
-
-
+        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -46,12 +54,12 @@ namespace Challenge_21_LPB
             int socialSecurity = int.Parse(textBoxSocialSecurity.Text);
             int phone = int.Parse(textBoxPhone.Text);
 
-            int startMiles = int.Parse(startMilesTextBox.Text);
-            int endMiles = int.Parse(endMilesTextBox.Text);
-            int milesTotal = endMiles - startMiles;
+            
 
 
 
+
+           
 
             //Format driver info
             string driverName = string.Format("Driver - {0}", textBoxName.Text);
@@ -60,14 +68,76 @@ namespace Challenge_21_LPB
 
             string phoneNumber = string.Format("Phone Number: {0: (000)000-0000} ", phone);
 
+
+
+            //Get Total Days out
+            TimeSpan daysOut = dateReturned - dateLeft;
+
+            int days = daysOut.Days;
+
+            string totalDays = string.Format("Total Days Out {0}", days);
+
+
+
+            //Get Days Off
+            int num1 = days;
+
+            int num2 = num1 % 5;
+
+            int num3 = num1 - num2;
+
+            int num4 = num3 / 5;
+
+            int vacationDays = num4 * 2;
+
+            string totalVacDays = string.Format("Vacation Days Earned: {0}", vacationDays);
+
+
+
+            // Get Total miles
+            int startMiles = int.Parse(startMilesTextBox.Text);
+            int endMiles = int.Parse(endMilesTextBox.Text);
+            int milesTotal = endMiles - startMiles;
             string totalMiles = string.Format("Total Miles: {0}", milesTotal);
 
+
+
+
+
+            //Get Total Pay
+             if (refridgeratedCheckBox.IsChecked == true)
+             {
+                double moneyEarned = milesTotal * 0.37;
+
+                string amountPaid = string.Format("Total Pay Due: {0:c}", moneyEarned);
+
+                resultlabel6.Content = amountPaid;
+            }
+             else
+             {
+                double moneyEarned = milesTotal * 0.25;
+
+                string amountPaid = string.Format("Total Pay Due: {0:c}", moneyEarned);
+
+                resultlabel6.Content = amountPaid;
+             }
+
+             
+             
+
+             
+
+
+
+
+
+             //Print ticket
             resultlabel1.Content = driverName;
             resultlabel2.Content = socialSecurityNumber;
             resultlabel3.Content = phoneNumber;
             resultlabel4.Content = totalMiles;
-
-
+            resultlabel5.Content = totalDays;
+            resultlabel7.Content = totalVacDays;
 
 
 
